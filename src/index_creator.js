5aa41f668dc6c16a684edbb0cbacc4b0285a60a5
Padmanabh Dabke <pdabke@nabh.com>
2022-02-18 18:50:18 -0800

    Package description is now added in the header of generated index file

--- src/index_creator.js
+++ src/index_creator.js
@@ -6,6 +6,20 @@ function createIndex(folder) {
   if (!folder) folder = ".";
   var files = fs.readdirSync(folder);
   var indexContent = "";
+
+  // Check if there is a package.json file in the current directory and extract 
+  // package documentation
+  if (fs.existsSync("package.json")) {
+    let pkgObj = JSON.parse(fs.readFileSync("package.json"));
+    if (pkgObj.description) {
+      indexContent += "/**" + os.EOL;
+      indexContent += " * " + pkgObj.description + os.EOL;
+      if (pkgObj.version) indexContent += " * Version " + pkgObj.version + os.EOL;
+      indexContent += " *" + os.EOL
+      indexContent += " * @packageDocumentation" + os.EOL
+      indexContent += " */" + os.EOL;
+    }
+  }
   for (let f of files) {
     if (f.endsWith(".ts") && !(f == "index.ts")) {
       indexContent = indexContent + "export * from \"./" + f.substring(0, f.length-3) + "\";" + os.EOL;
--- tests/index_creator.test.js
+++ tests/index_creator.test.js
@@ -1,54 +1,27 @@
 "use strict"
 const fs = require("fs");
 const path = require("path");
+const process = require("process");
 const createIndex = require("../src/index_creator");
 
-const TEST_1 = 
-`
-export class Test1 {
-
-}
-`;
-
-const TEST_2 = 
-`
-export class Test2 {
-
-}
-`;
-const INDEX_CONTENT =
-`
-export * from "./Test1";
-export * from "./Test2";
-`;
-
 beforeAll( () => {
-  setupTargetFiles();
+  process.chdir(path.resolve(__dirname));
+  if (fs.existsSync(path.resolve(__dirname, "target", "index.ts"))) {
+    fs.rmSync(path.resolve(__dirname, "target", "index.ts"));
+  }
 });
 
 afterAll( () => {
-  cleanupTargetFiles();
+  if (fs.existsSync(path.resolve(__dirname, "target", "index.ts"))) {
+    fs.rmSync(path.resolve(__dirname, "target", "index.ts"));
+  }
 });
+
 
 test("Create index.ts", () => {
   createIndex(path.resolve(__dirname, "target"));
   let indexContent = fs.readFileSync(path.resolve(__dirname, "target", "index.ts"), { encoding: "utf-8"});
+  let refContent = fs.readFileSync("expected_index.txt", {encoding: "utf-8"});
   indexContent = indexContent.trim().replace(/\s/gm, '');
-  expect(indexContent.trim()).toBe(INDEX_CONTENT.trim().replace(/\s/gm, ''));
+  expect(indexContent.trim()).toBe(refContent.trim().replace(/\s/gm, ''));
 });
-
-function setupTargetFiles() {
-  let targetDir = path.resolve(__dirname, "target");
-  // check if target directory exists
-  if (fs.existsSync(targetDir)) {
-    fs.rmdirSync(targetDir, { recursive: true, force: true});
-  }
-
-  fs.mkdirSync(targetDir);
-  fs.writeFileSync(path.join(targetDir, "Test1.ts"), TEST_1);
-  fs.writeFileSync(path.join(targetDir, "Test2.ts"), TEST_2);
-}
-
-function cleanupTargetFiles() {
-  fs.rmdirSync(path.resolve(__dirname, "target"), { recursive: true, force: true});  
-}
